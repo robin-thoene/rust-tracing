ad445b8028de9b8e6ac3c384baf729251522197b
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-03-17 12:38:06 +0100

    create simple axum api

---
+++ axum-api/src/main.rs
@@ -0,0 +1,19 @@
+use axum::{extract::Path, routing::get, Router};
+
+async fn greet_handler(Path((first_name, last_name)): Path<(String, String)>) -> String {
+    format!(
+        "Hello {} {}, the axum-api server greets you!",
+        first_name, last_name
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/greet/:first_name/:last_name", get(greet_handler));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:5000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+    println!("Hello, world!");
+}
