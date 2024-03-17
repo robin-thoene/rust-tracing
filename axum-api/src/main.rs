f9b28523b5ad9996614936fe7d5ce2e90312ef1a
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-03-17 12:56:47 +0100

    add a basic tracing middleware to the axum-api

--- axum-api/src/main.rs
+++ axum-api/src/main.rs
@@ -1,4 +1,47 @@
-use axum::{extract::Path, routing::get, Router};
+use axum::{
+    extract::{Path, Request},
+    middleware::{self, Next},
+    response::Response,
+    routing::get,
+    Router,
+};
+use opentelemetry::{
+    global::{self, shutdown_tracer_provider},
+    trace::{Span, SpanKind, TraceError, Tracer},
+    KeyValue,
+};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{runtime, trace, Resource};
+
+fn init_tracer() -> Result<opentelemetry_sdk::trace::Tracer, TraceError> {
+    opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .with_trace_config(
+            trace::config().with_resource(Resource::new(vec![KeyValue::new(
+                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                "axum-api",
+            )])),
+        )
+        .install_batch(runtime::Tokio)
+}
+
+async fn otel_tracing_middleware(request: Request, next: Next) -> Response {
+    let tracer = global::tracer("tracing-jaeger");
+    let mut span = tracer
+        .span_builder("todo-replace")
+        .with_kind(SpanKind::Server)
+        .start(&tracer);
+    // TODO: handle tracing that is related to the request.
+    let response = next.run(request).await;
+    span.end();
+    // TODO: handle tracing that is related to the response.
+    response
+}
 
 async fn greet_handler(Path((first_name, last_name)): Path<(String, String)>) -> String {
     format!(
@@ -9,11 +52,16 @@ async fn greet_handler(Path((first_name, last_name)): Path<(String, String)>) ->
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/greet/:first_name/:last_name", get(greet_handler));
+    let _tracer = init_tracer().expect("Failed to initialize tracer.");
+
+    let app = Router::new()
+        .route("/greet/:first_name/:last_name", get(greet_handler))
+        .layer(middleware::from_fn(otel_tracing_middleware));
     let listener = tokio::net::TcpListener::bind("127.0.0.1:5000")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
-    println!("Hello, world!");
+
+    shutdown_tracer_provider();
 }
