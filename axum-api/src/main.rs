45ee29d09db87998a94cd1355ff951d6557e86c9
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-03-20 21:04:14 +0100

    - improve tracing of header data, - add http method - add response status

--- axum-api/src/main.rs
+++ axum-api/src/main.rs
@@ -2,14 +2,12 @@ use std::net::SocketAddr;
 
 use axum::{
     extract::{ConnectInfo, Path},
-    http::{
-        header::{HOST, USER_AGENT},
-        HeaderMap, Request,
-    },
+    headers::{Host, UserAgent},
+    http::{HeaderMap, Request},
     middleware::{self, Next},
     response::Response,
     routing::get,
-    Router,
+    Router, TypedHeader,
 };
 use opentelemetry::{
     global::{self, shutdown_tracer_provider},
@@ -39,6 +37,8 @@ fn init_tracer() -> Result<opentelemetry_sdk::trace::Tracer, TraceError> {
 
 async fn otel_tracing_middleware<B>(
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    TypedHeader(user_agent): TypedHeader<UserAgent>,
+    TypedHeader(host): TypedHeader<Host>,
     headers: HeaderMap,
     request: Request<B>,
     next: Next<B>,
@@ -51,7 +51,6 @@ async fn otel_tracing_middleware<B>(
         .span_builder("todo-replace")
         .with_kind(SpanKind::Server)
         .start_with_context(&tracer, &parent_cx);
-    // TODO: handle tracing that is related to the request.
     // Set some of the conventional span attributes.
     span.set_attributes(vec![
         KeyValue::new(
@@ -66,22 +65,32 @@ async fn otel_tracing_middleware<B>(
             opentelemetry_semantic_conventions::trace::URL_PATH,
             request.uri().path().to_string(),
         ),
+        KeyValue::new(
+            opentelemetry_semantic_conventions::trace::HTTP_REQUEST_METHOD,
+            request.method().to_string(),
+        ),
+        KeyValue::new(
+            opentelemetry_semantic_conventions::trace::USER_AGENT_ORIGINAL,
+            user_agent.to_string(),
+        ),
+        KeyValue::new(
+            opentelemetry_semantic_conventions::trace::SERVER_ADDRESS,
+            host.hostname().to_string(),
+        ),
     ]);
+    // If the host port can be determined, trace it.
+    if let Some(port) = host.port() {
+        span.set_attribute(KeyValue::new(
+            opentelemetry_semantic_conventions::trace::SERVER_PORT,
+            port.to_string(),
+        ));
+    }
     // If the scheme can be parsed, trace it.
     if let Some(scheme_str) = &request.uri().scheme_str() {
         span.set_attribute(KeyValue::new(
             opentelemetry_semantic_conventions::trace::URL_SCHEME,
             scheme_str.to_string(),
         ));
-    }
-    // If the request headers contain the user agent header, trace it.
-    if let Some(user_agent_header) = &headers.get(USER_AGENT) {
-        if let Ok(user_agent_header) = user_agent_header.to_str() {
-            span.set_attribute(KeyValue::new(
-                opentelemetry_semantic_conventions::trace::USER_AGENT_ORIGINAL,
-                user_agent_header.to_string(),
-            ));
-        }
     }
     // If the request contains a query, add it to the span as well.
     if let Some(query) = request.uri().query() {
@@ -90,27 +99,13 @@ async fn otel_tracing_middleware<B>(
             query.to_string(),
         ));
     }
-    // If the request contains the host header, parse the host domain/ip and the port.
-    if let Some(host_header) = &headers.get(HOST) {
-        if let Ok(host_header) = host_header.to_str() {
-            let split: Vec<&str> = host_header.split(':').collect();
-            if let Some(server_address) = split.get(0) {
-                span.set_attribute(KeyValue::new(
-                    opentelemetry_semantic_conventions::trace::SERVER_ADDRESS,
-                    server_address.to_string(),
-                ));
-            }
-            if let Some(server_port) = split.get(1) {
-                span.set_attribute(KeyValue::new(
-                    opentelemetry_semantic_conventions::trace::SERVER_PORT,
-                    server_port.to_string(),
-                ));
-            }
-        }
-    }
     // Process the request.
     let response = next.run(request).await;
-    // TODO: handle tracing that is related to the response.
+    // Add response related data to the span.
+    span.set_attribute(KeyValue::new(
+        opentelemetry_semantic_conventions::trace::HTTP_RESPONSE_STATUS_CODE,
+        response.status().to_string(),
+    ));
     // End the span.
     span.end();
     // Return the response.
