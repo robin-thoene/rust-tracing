49e3f2e6755431f688b29e91562622bb3c413687
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-06-17 16:48:21 +0200

    configure the first page to display the trace id

--- nextjs-frontend/src/app/weatherforecast/page.tsx
+++ nextjs-frontend/src/app/weatherforecast/page.tsx
@@ -1,3 +1,20 @@
 export default async function Weather() {
-  return <></>;
+  const response = await fetch("http://localhost:5240/weatherforecast", {
+    cache: "no-store",
+  });
+  const traceId = response.headers.get("trace_id");
+  console.log(traceId);
+  let dataToDisplay;
+  if (response.status == 200) {
+    dataToDisplay = JSON.stringify(await response.json());
+  } else {
+    dataToDisplay = "An error occured!";
+  }
+  return (
+    <div className="flex flex-col">
+      <div>{dataToDisplay}</div>
+      <div className="mt-6">Check out the traces using this id</div>
+      <div className="font-bold">{traceId}</div>
+    </div>
+  );
 }
